6bd9332c808943e9ec9278c7a64391fb759124e0
lordik <jetlord@mail.ru>
2023-02-08 10:38:41 +0300

    2

---
+++ JSON и  XML/cat.js
@@ -0,0 +1,21 @@
+mans = 
+{
+    [
+        {
+            "name": "Roman",
+            "age": "19",
+            "city": "MSK",
+        },
+        {
+            "name": "Alex",
+            "age": "18",
+            "city": "SPB",
+        },
+    ]
+},
+
+mans.forEach((man) => {
+    <td>
+        <tr>man.name</tr>
+    </td>
+})
